f4ecbfbf07a8f8485b2b5e3f9c9de8a92987d505
John Falcone <falcone@umich.edu>
2018-09-18 12:45:59 -0400

    Final

--- SI364F18_HW1.py
+++ SI364F18_HW1.py
@@ -6,22 +6,20 @@
 
 ## List below here, in a comment/comments, the people you worked with on this assignment AND any resources you used to find code (50 point deduction for not doing so). If none, write "None".
 
+##StackOverflow
 
 
 ## [PROBLEM 1] - 150 points
 ## Below is code for one of the simplest possible Flask applications. Edit the code so that once you run this application locally and go to the URL 'http://localhost:5000/class', you see a page that says "Welcome to SI 364!"
 
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 app.debug = True
 
-@app.route('/')
+@app.route('/class')
 def hello_to_you():
-    return 'Hello!'
+    return 'Welcome to SI 364!'
 
-
-if __name__ == '__main__':
-    app.run()
 
 
 ## [PROBLEM 2] - 250 points
@@ -39,11 +37,46 @@ if __name__ == '__main__':
 
 ## Run the app locally (repeatedly) and try these URLs out!
 
+import requests
+import json
+
+@app.route('/movie/<movie_name>')
+def get_movie_data(movie_name):
+
+    #movie_data = requests.get('http://itunes.apple.com/search?', params = {'term': movie_name, 'entity': 'movie'})
+    movie_data = requests.get('https://itunes.apple.com/search?term=' + movie_name)
+    return movie_data.text
+
 ## [PROBLEM 3] - 250 points
 
 ## Edit the above Flask application code so that if you run the application locally and got to the URL http://localhost:5000/question, you see a form that asks you to enter your favorite number.
 ## Once you enter a number and submit it to the form, you should then see a web page that says "Double your favorite number is <number>". For example, if you enter 2 into the form, you should then see a page that says "Double your favorite number is 4". Careful about types in your Python code!
 ## You can assume a user will always enter a number only.
+
+@app.route('/question/')
+def user_input():
+    number = """<!DOCTYPE html>
+<html>
+<body>
+<form action="http://localhost:5000/double_your_num" method="post">
+  Enter Your Favorite Number!<br>
+  <br></br>
+  <input type="text" name="number">
+  <br>
+  <br></br>
+  <input type="submit" value="Submit">
+</form>
+</body>
+</html>"""
+    return number
+
+@app.route('/double_your_num',methods=['POST', 'GET'])
+def double_number():
+  if request.method == 'POST':
+    user_num = str(request.form['number'])
+    doubled = int(user_num)*2
+    return "<p>Double your favorite number is {}</p>".format(str(doubled))
+
 
 
 ## [PROBLEM 4] - 350 points
@@ -65,3 +98,61 @@ if __name__ == '__main__':
 # You can assume that a user will give you the type of input/response you expect in your form; you do not need to handle errors or user confusion. (e.g. if your form asks for a name, you can assume a user will type a reasonable name; if your form asks for a number, you can assume a user will type a reasonable number; if your form asks the user to select a checkbox, you can assume they will do that.)
 
 # Points will be assigned for each specification in the problem.
+import re
+
+@app.route('/numbers',methods=["POST","GET"])
+## The user will use this tool to find fun facts about different numbers that they may find interesting. This uses the open-source Numbers API, which takes an integer as input and can return fun facts about numbers, dates, or years.
+def numbers_call():
+  alt = """
+  <!DOCTYPE html>
+  <html>
+  <body>
+  <form action="http://localhost:5000/numbers" method='POST'>
+    What kind of Number would you like to enter?<br>
+    <input type="checkbox" name="integer" value="integter"> Integer<br>
+    <input type="checkbox" name="date" value="date"> Date<br>
+    <input type="checkbox" name="year" value="year"> Year<br>
+    <input type="checkbox" name="other" value="other"> Other<br>
+    Enter the number here: <br><input type="text" name="input"<br>
+    <input type="submit" value="Submit">
+  </form>
+</body>
+</html>
+"""
+  if request.method == "POST":
+
+
+    user_input = str(request.form['input'])
+
+
+    number_info = requests.get('http://numbersapi.com/' + user_input + "?json")
+    number_json = json.loads(number_info.text)
+    number_text = json.dumps(number_json)
+
+    #number_indexed = findall('"([^"]*)"', number_text)
+    number_indexed = str(re.findall(r'"(.*?)"', number_text))
+    #number_indexed = number_text[3]
+    textlist = number_text.split(',')
+    index = textlist[0] 
+    number_indexed_2 = str(re.findall(r'"(.*?)"', index))
+    textlist_2 = number_indexed_2.split(',')
+    index2 = textlist_2[1]
+    index2 = index2.replace("\"", "")
+    index2 = index2.replace("\'", "")
+    index2 = index2.replace("]", "")
+
+
+
+    return "Here is an interesting fact about {}:       ".format(user_input) + index2
+    #return "Here:"
+    #return type(number_info)
+    #print(number_json)
+    #return number_json
+    #return number_json
+  else:
+  	return alt
+
+
+
+if __name__ == '__main__':
+    app.run()
